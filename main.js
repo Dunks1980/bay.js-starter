c7e16a2a38e7a89fb48fcad4ad0cd34471b7b8d8
Ian Dunkerley <Dunks1980@users.noreply.github.com>
2023-01-23 21:20:11 +0000

    Tweaks

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ import start_page from "./templates/start_page.html?raw";
 
 bay();
 bay.create("start-page", start_page, ["prop-color"]);
-bay.create("header-component", header_el, ["hide-logo", "maxwidth"]);
+bay.create("header-component", header_el, ["maxwidth"]);
 bay.create("logo-component", logo_el, ["prop-color"]);
